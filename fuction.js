fadca92f6940dd25a2b2544639990494e92841c2
Altafs01 <altafshaikh1697@gmail.com>
2023-09-22 22:37:16 +0530

    changes i have made

--- fuction.js
+++ fuction.js
@@ -156,7 +156,7 @@
 //     if (((year % 4 ===0 && year % 100 !==0) || year % 400===0)){
 //         console.log(`${year} is Leap year`);
 //     }
-    
+
 //     else{
 //         console.log(`${year} is not Leap year`);
 //     }
@@ -168,4 +168,208 @@
 // checkLeapYear(2014);
 // checkLeapYear(2015);
 // checkLeapYear(2016);
+// checkLeapYear(2018);
 // -----------------------------------------------------------------------------------------------------------
+
+// function sumOfMultipleValue(num){
+//     let sumOfMultipleValue = 0;
+
+//     for (let i=0; i<=num; i++){
+
+//         if (i % 3===0 || i % 5===0){
+//             console.log(i);
+//             sumOfMultipleValue +=i;
+//         }
+//         console.log(`sumOfMultipleValue of 3 & 5 upto ${num}  is `, sumOfMultipleValue);
+//     }
+// }
+// sumOfMultipleValue(12);
+// // -----------------------------------------------------------------------------------------------------------------
+
+// function sumOfMultipleValue(curLimit){
+
+//     let sumOfMultipleValue = 0;
+
+//     for( let i= 1; i <= curLimit; i++){
+//         if(i % 3 ===0 || i % 5 ===0){
+//             // console.log(i);
+//             sumOfMultipleValue +=i;
+//         }
+//     }
+//     console.log(`sumOfMultipleValue of 3 & 5 upto ${curLimit} digit is:`, sumOfMultipleValue)
+// }
+
+// sumOfMultipleValue(10);
+// ---------------------------------------------------------------------------------------------------------------------------
+
+
+// let rows = 10;
+
+// // pattern variable carries the final pattern in string format
+// let pattern = "";
+
+// // outer loop runs for `rows` no. of times
+// for (let n = 1; n <= rows; n++) {
+//    // inner loop runs for n
+//    for (let num = 1; num <= n; num++) {
+//       pattern += num;
+//    }
+
+//    // Add a new line character after contents of each line
+//    pattern += "\n";
+// }
+// console.log(pattern);
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// var changetext = document. getElementById(`changetext`);
+// console.log(changetext);
+// headertitle.textContent =`change`
+
+
+// function sumOfMultipleValue(num){
+//    if (num % 2 ===0 & num % 4 ===0){
+//       return (`The num ${num} is divisible by 2 & 4 `);
+//    }
+
+//    else {
+//       return (`The num ${num} is not divisible by 2 & 4 `);
+//    }
+// }
+
+// check1 = sumOfMultipleValue(20);
+// console.log(check1);
+// ---------------------------------------------------------------------------------------------------------
+
+// function isSpeedLimit(speed){
+//    if (speed < 70){
+//       return (`Good safe driwing`);
+//    }
+
+//    if (speed > 70){
+//       let a =0;
+//       b = (speed - 70) /5;
+
+//       if(speed > 120){
+//          return (`Your liecense cancelled`);
+//       }
+//       else {
+//          return (`your penality point is ` + b);
+//       }
+//    }
+// }
+
+// check = isSpeedLimit (65);
+// console.log(check);
+// check1 = isSpeedLimit (80);
+// console.log(check1);
+// check2 = isSpeedLimit (130);
+// console.log(check2);
+// ----------------------------------------------------------------------------------------------
+
+
+// function isSpeedLimit(speed) {
+//     if (speed < 70) {
+//         // console.log(`Good safe driwing`);
+//         return (`Good Safe Driwing`);
+//     }
+
+//     if (speed > 70) {
+//         a = 0;
+//         b = (speed - 70) / 5;
+//         // console.log(b);
+
+//         if (b > 9) {
+//             return (`Your licence is cencelled`);
+//         }
+
+//         else {
+//             return (`your pentally point is ` + b);
+//         }
+
+//     }
+
+
+// }
+
+// // check = isSpeedLimit (69);
+// // console.log (check);
+// // check1 = isSpeedLimit (80);
+// // console.log (check1);
+// check2 = isSpeedLimit (115);
+// console.log (check2);
+// ------------------------------------------------------------------------------------------------------------------
+
+// function checkAge(age) {
+//     if (age >= 18) {
+//         // console.log (`Permission Granteed`);
+//         return true;
+//     }
+
+//     else {
+//         return confirm (`You need to get permission to your parent`);
+//     }
+// }
+
+// let age = prompt(`How old are You `)
+
+// if (checkAge(age)) {
+//     alert(`access granted`);
+// }
+
+// else {
+//     alert(`access denied`);
+// }
+
+
+// check = checkAge (17);
+// alert (check);
+// check1 = checkAge(20);
+// alert (check1);
+// ------------------------------------------------------------------------------------------------------------------
+
+// function sum (a,b){
+//     return a + b;
+// }
+
+// function sub (c,d){
+//     return c - d;
+// }
+
+// function mul (e,f){
+//     return e * f;
+// }
+
+// function div (g,h){
+//     return g / h;
+// }
+
+//  let firstNumber = prompt (`enter your first no.`);
+// let secondNumber = prompt (`enter your second no.`);
+
+// firstNumber = Number (firstNumber);
+// secondNumber = Number (secondNumber);
+// console.log (typeof(firstNumber));
+// console.log (typeof(secondNumber));
+
+// let Addition = sum(firstNumber, secondNumber);
+// Additionresult = firstNumber + secondNumber;
+// alert ("The result of addition is  " + Additionresult);
+
+// let substraction = sub (firstNumber, secondNumber);
+// subtractionresult = firstNumber - secondNumber;
+// alert (`The result of substraction is ` + subtractionresult);
+// ------------------------------------------------------------------------------------------------------------------------
+
+function showMessage (){
+    let message = 'Hello Altaf';
+    console.log(message);
+}
+
+let greeter = `hi`;
+let hello = `hey hi`;
+let times = 4;
+
+if (times > 3){
+    let hello = "say hello instead";
+    console.log(hello);
+}
